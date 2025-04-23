9204d62750f7629bc2dd906dcf0e0f92af5cab17
Brian Muñoz <brozs16@gmail.com>
2025-04-23 05:31:26 -0300

    Primer commit

---
+++ src/main/java/com/restexplorer/app/rest/controller/TodoController.java
@@ -0,0 +1,15 @@
+package com.restexplorer.app.rest.controller;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class TodoController {
+    private String
+
+
+}
